d52b4116dbd7a9ca1f971f18e0453661a9448d98
Shift <shift@laravelshift.com>
2023-06-09 17:45:21 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them and merged your true customizations -
    where ENV variables may not be used.

--- config/filesystems.php
+++ config/filesystems.php
@@ -15,17 +15,6 @@ return [
 
     'default' => env('FILESYSTEM_DISK', 'local'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Cloud Filesystem Disk
-    |--------------------------------------------------------------------------
-    |
-    | Many applications store files both locally and in the cloud. For this
-    | reason, you may specify a default "cloud" driver here. This driver
-    | will be bound as the Cloud disk implementation in the container.
-    |
-    */
-
     'cloud' => env('FILESYSTEM_CLOUD', 's3'),
 
     /*
@@ -35,7 +24,7 @@ return [
     |
     | Here you may configure as many filesystem "disks" as you wish, and you
     | may even configure multiple disks of the same driver. Defaults have
-    | been setup for each driver as an example of the required options.
+    | been set up for each driver as an example of the required values.
     |
     | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
@@ -58,6 +47,7 @@ return [
             ],
             'url' => env('APP_URL'),
             'visibility' => 'public',
+            'throw' => false,
         ],
 
         'publicmedia' => [
@@ -99,6 +89,7 @@ return [
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
+            'throw' => false,
         ],
 
         'content' => [
@@ -114,6 +105,8 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
         ],
 
     ],
