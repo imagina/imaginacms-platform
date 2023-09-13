5eb0ab00bfba19c12eff018de27439335d5fce79
Juan Escalante <jcec007@gmail.com>
2023-09-13 17:18:01 -0500

    Update: update ImageHandlerInterface.php

--- Modules/Media/Image/ImageHandlerInterface.php
+++ Modules/Media/Image/ImageHandlerInterface.php
@@ -11,5 +11,5 @@ interface ImageHandlerInterface
      * @param  array  $options
      * @return \Intervention\Image\Image
      */
-    public function handle($image, $options);
+    public function handle(Image $image, array $options): Image;
 }
