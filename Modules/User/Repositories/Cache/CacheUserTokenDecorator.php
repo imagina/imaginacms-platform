265b9306d6b59dedeb2a051916e778d517a60324
Omar Chunza <odchunzag.une@gmail.com>
2023-09-14 11:17:42 -0500

    Correccion de error durante instalacion.

--- Modules/User/Repositories/Cache/CacheUserTokenDecorator.php
+++ Modules/User/Repositories/Cache/CacheUserTokenDecorator.php
@@ -24,7 +24,7 @@ class CacheUserTokenDecorator extends BaseCacheDecorator implements UserTokenRep
     /**
      * Get all tokens for the given user
      */
-    public function allForUser(int $userId): Collection
+    public function allForUser($userId)
     {
         $this->remember(function () use ($userId) {
             return $this->repository->allForUser($userId);
